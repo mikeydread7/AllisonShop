67dc967fa95a8a9cd277df7d7d7c8c5c1b1722ce
Jamaican.creator <mike1somers@gmail.com>
2019-04-24 06:44:07 -0400

    Update ReversalSchedular.java

--- src/ReversalSchedular.java
+++ src/ReversalSchedular.java
@@ -1,17 +1,18 @@
-/**
- * The purpose of this  Class
- */
 package com.allison.shop.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author MichaelDev
  *
  */
 public class ReversalSchedular extends BatchSchedular {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	public ReversalSchedular() {
 		super(ACHBatchType.REVERSAL);
 	}
 	public void sheduleCurrentBatchInQueue(final ACHSchedularEntity achSchedularEntity) {
-		logger.info("{}: schedule entity properties {}",this.achBatchType, achSchedularEntity.getProfileId());
+		logger.info("{}: Exception to the norms {}",this.achBatchType, achSchedularEntity.getProfileId());
         }
 }
